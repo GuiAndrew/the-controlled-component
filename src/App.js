1d09cc91bdf635c8fd3de7d02a6fbb7bb38245e0
GuiAndrew <gfilipea001@gmail.com>
2020-01-18 11:35:29 +0000

    React 4 - The controlled component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import FormComponent from './components/formcomponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {value: ''};
+  } 
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  } 
+
+  render(){
+    return (
+      <div>
+        <form className="App">
+          <label htmlFor="value">Value</label>
+          <input id="name" type="text" value={this.state.value} onChange={(event) =>{this.handleChange(event)}}/>
+        </form>
+        <FormComponent 
+          value = { this.state.value } 
+        />
+      </div>
+    );
+  } 
 }
 
 export default App;
---
+++ src/components/formcomponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FormComponent(props) {
+  return <h1>{props.value}</h1>;
+}
+
+export default FormComponent;
